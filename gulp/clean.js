8fbb6f26d5286e7df5cf99c3180f03828949dd5a
kk1230t <kk1230t@naver.com>
2022-11-24 06:32:31 +0900

    122

---
+++ gulp/clean.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import path from 'path';
+import del from 'del';
+
+const clean = ({
+  gulp,
+  config,
+  taskTarget,
+}) => {
+  const dir = config.directory;
+  // clean development project
+  gulp.task('clean:development', () => {
+    del.bind(null, [path.join(dir.development)])
+  
+  });
+
+  // clean development project
+  gulp.task('clean:production', () => {
+    console.log('prod')
+    del.bind(null, [path.join(taskTarget)])
+  });
+
+  // clean development project
+  gulp.task('clean:ghpages', () => {
+    console.log('ghp')
+    del.bind(null, [path.join(dir.ghpages)])
+  });
+};
+
+export default clean;
