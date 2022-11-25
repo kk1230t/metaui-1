f901998a7aa30a667e67bc4ba71559e528b0c1f6
ghilchaekim <kk1230t@gmail.com>
2022-11-25 17:05:44 +0900

    11

---
+++ gulp/watch.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const watch = ({gulp, plugins, args, config, browserSync, taskTarget}) => {
+    const dir = config.directory;
+    // console.log(dir)
+
+    console.log(taskTarget)
+
+    gulp.task('watch', () => {
+        // console.clear();
+        console.log('watch')
+
+        browserSync.init({
+            server: taskTarget,
+            notify: false,
+            plugins: ['bs-eslint-message'],
+            open: config.autoLaunchBrowser
+        })
+    })
+}
+
+export default watch;
