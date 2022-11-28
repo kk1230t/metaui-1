2f11f9b6fdd03ed4375f86e0b801d21a32192bac
kk1230t <kk1230t@naver.com>
2022-11-28 21:47:35 +0900

    112

---
+++ gulp/tasks/plugins.js
@@ -0,0 +1,9 @@
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const plugins = gulpLoadPlugins({
+  // when set to true, the plugin will log info to console.
+  // Useful for bug reporting and issue debugging
+  DEBUG: false
+});
+
+export default plugins;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,15 @@ require("@babel/register")
 
 import {series, parallel} from "gulp";
 import clean from './gulp/tasks/clean'
+import pug from './gulp/tasks/pug'
 import {config} from "./gulp/config"
 
 
+
+// javascript(taskOptionList);
+// sass(taskOptionList);
+// template(taskOptionList);
+// watch(taskOptionList);
 
 
 
@@ -14,6 +20,9 @@ import {config} from "./gulp/config"
 //   return console.log(config11);
 // }
 
-const cleanRoot2 = series(clean)
+const cleanRoot2 = series(
+  clean,
+  pug
+)
 
 exports.build = cleanRoot2;
